6c7b679788e95a8bcc9b41c9ade7fb7bcd321a04
wimgonza <167358036+wimgonza@users.noreply.github.com>
2025-01-20 22:24:10 -0800

    Attempt #1 at imitating in Experiment #2

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -1,79 +1,72 @@
 // sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: William Gonzalez
+// Date: 1/20/25
 
 // Here is how you might set up an OOP p5.js project
 // Note that p5.js looks for a file called sketch.js
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
 // Globals
-let myInstance;
-let canvasContainer;
-var centerHorz, centerVert;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
-
-function resizeScreen() {
-  centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
-  centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
-  console.log("Resizing...");
-  resizeCanvas(canvasContainer.width(), canvasContainer.height());
-  // redrawCanvas(); // Redraw everything based on new size
-}
+// Symmetry corresponding to the number of reflections. Change the number for different number of reflections 
+let symmetry = 10;
+let angle = 360 / symmetry;
+let clearButton, mouseButton, keyboardButton;
+let slider;
 
 // setup() function is called once when the program starts
 function setup() {
-  // place our canvas, making it fit our container
-  canvasContainer = $("#canvas-container");
-  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-  canvas.parent("canvas-container");
-  // resize canvas is the page is resized
+  // Creating the canvas and making it gray
+  createCanvas(710, 710);
+  angleMode(DEGREES);
+  background(127);
 
-  // create an instance of the class
-  myInstance = new MyClass("VALUE1", "VALUE2");
+  // Creating the clear screen button
+  clearButton = createButton('clear');
+  clearButton.mousePressed(clearScreen);
 
-  $(window).resize(function() {
-    resizeScreen();
-  });
-  resizeScreen();
+  // Creating the button for Full Screen
+  fullscreenButton = createButton('Full Screen');
+  fullscreenButton.mousePressed(screenFull);
+
+  // Setting up the slider for the thickness of the brush
+  brushSizeSlider = createButton('Brush Size Slider');
+  sizeSlider = createSlider(1, 32, 4, 0.1);
+}
+
+// Clears the screen by setting it back to gray
+function clearScreen() {
+  background(127);
+}
+
+// Full Screen Function
+function screenFull() {
+  let fs = fullscreen();
+  fullscreen(!fs);
 }
 
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
-  background(220);    
-  // call a method on the instance
-  myInstance.myMethod();
+  // Setting up the kaleidoscope drawing portion
+  translate(width / 2, height / 2);
 
-  // Set up rotation for the rectangle
-  push(); // Save the current drawing context
-  translate(centerHorz, centerVert); // Move the origin to the rectangle's center
-  rotate(frameCount / 100.0); // Rotate by frameCount to animate the rotation
-  fill(234, 31, 81);
-  noStroke();
-  rect(-125, -125, 250, 250); // Draw the rectangle centered on the new origin
-  pop(); // Restore the original drawing context
-
-  // The text is not affected by the translate and rotate
-  fill(255);
-  textStyle(BOLD);
-  textSize(140);
-  text("p5*", centerHorz - 105, centerVert + 40);
-}
-
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+  if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
+    let mx = mouseX - width / 2;
+    let my = mouseY - height / 2;
+    let pmx = pmouseX - width / 2;
+    let pmy = pmouseY - height / 2;
+    
+    if (mouseIsPressed) {
+      for (let i = 0; i < symmetry; i++) {
+        rotate(angle);
+        let sw = sizeSlider.value();
+        stroke(0,0,255);
+        strokeWeight(sw);
+        line(mx, my, pmx, pmy);
+        push();
+        scale(1, -1);
+        line(mx, my, pmx, pmy);
+        pop();
+      }
+    }
+  }
+  
 }
