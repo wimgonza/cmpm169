a0e2f3c5eb42fde070d3fc1763473f9d7034118c
wimgonza <167358036+wimgonza@users.noreply.github.com>
2025-01-27 23:38:33 -0800

    Cleaning up the comments

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -26,24 +26,22 @@ function draw() {
 
   // Create a random number of snowflakes each frame
   for (let i = 0; i < random(5); i++) {
-    snowflakes.push(new snowflake()); // append snowflake object
+    snowflakes.push(new snowflake());
   }
 
-  // loop through snowflakes with a for..of loop
   for (let flake of snowflakes) {
     if (mouseIsPressed) {
-      flake.moveTowardsMouse(t); // Move towards mouse when pressed
-      flake.rotateAroundMouse(t); // Start rotating around mouse as they approach
+      flake.moveTowardsMouse(t);
+      flake.rotateAroundMouse(t);
     } else {
-      flake.update(t); // update snowflake position
+      flake.update(t);
     }
-    flake.display(); // draw snowflake
+    flake.display();
   }
 }
 
 // snowflake class
 function snowflake() {
-  // initialize coordinates
   this.posX = 0;
   this.posY = random(-50, 0);
   this.initialangle = random(0, 2 * PI);
@@ -87,11 +85,9 @@ function snowflake() {
     ellipse(this.posX, this.posY, this.size);
   };
 
-  // Method to move snowflakes towards the mouse
   this.moveTowardsMouse = function(time) {
     let distanceToMouse = dist(this.posX, this.posY, mouseX, mouseY);
 
-    // Move snowflake towards the mouse position
     if (distanceToMouse > 1) {
       let angleToMouse = atan2(mouseY - this.posY, mouseX - this.posX);
       let moveSpeed = map(distanceToMouse, 0, width, 0, 0.5); // move faster when far, slower when close
@@ -100,25 +96,20 @@ function snowflake() {
     }
   };
 
-  // Method to rotate the snowflake around the mouse as it gets closer
   this.rotateAroundMouse = function(time) {
     let distanceToMouse = dist(this.posX, this.posY, mouseX, mouseY);
 
-    // Gradually increase rotation factor as snowflake gets closer to mouse
-    if (distanceToMouse < 75) { // Smaller radius (distance threshold) for swirling
-      this.rotationFactor = lerp(this.rotationFactor, 1, transitionSpeed); // Gradually increase rotation factor
+    if (distanceToMouse < 75) { 
+      this.rotationFactor = lerp(this.rotationFactor, 1, transitionSpeed);
     }
 
-    // If rotationFactor is > 0, start rotating around the mouse
     if (this.rotationFactor > 0) {
-      // Randomize the rotation pattern (not a perfect circle)
-      let angleOffset = random(TWO_PI); // random angle to vary the rotation
-      let baseRadius = map(distanceToMouse, 0, 75, 50, 10); // Vary radius as it gets closer
-      let radius = baseRadius + this.randomOffset + this.initialRadius; // Apply random offset and initial radius
+      let angleOffset = random(TWO_PI);
+      let baseRadius = map(distanceToMouse, 0, 75, 50, 10);
+      let radius = baseRadius + this.randomOffset + this.initialRadius;
 
-      let angle = time * 0.1 + this.initialangle + angleOffset;  // Add random variation to the rotation angle
+      let angle = time * 0.1 + this.initialangle + angleOffset;
 
-      // Update the snowflake's position to follow a swirling motion around the mouse
       this.posX = mouseX + radius * cos(angle);
       this.posY = mouseY + radius * sin(angle);
     }
