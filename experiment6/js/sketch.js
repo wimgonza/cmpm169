b3de1eb2e2d91e845a059cd31ab5ccc2abfa6bb9
wimgonza <167358036+wimgonza@users.noreply.github.com>
2025-02-19 00:22:00 -0800

    Attempt #8

--- experiment6/js/sketch.js
+++ experiment6/js/sketch.js
@@ -34,7 +34,7 @@ function setup() {
   noLoop();
   background(0);
 
-  opentype.load('./data/FreeSansNoPunch.otf', function(err, f) {
+  opentype.load('experiment6/data/FreeSansNoPunch.otf', function(err, f) {
     if (err) {
       print(err);
     } else {
